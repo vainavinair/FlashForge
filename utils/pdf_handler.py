bc792d86f3f298c5b967bfc0f004fee242e7d790
vainavinair <vainavinair2009558@gmail.com>
2025-08-07 23:31:28 +0530

    remove layout

--- utils/pdf_handler.py
+++ utils/pdf_handler.py
@@ -1,5 +1,4 @@
 import pdfplumber
-from .layout_detect import extract_text_by_layout
 
 def load_pdf(uploaded_file):
     try:
@@ -11,7 +10,7 @@ def load_pdf(uploaded_file):
 def extract_text_all_pages(pdf):
     all_text = ""
     for page in pdf.pages:
-        text = extract_text_by_layout(page)
+        text = page.extract_text()
         if text:
             all_text += text + "\n"
     return all_text.strip()
